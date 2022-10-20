2b79db3620de0da9141f42b7edef52dc93f936b3
Oles Tkachuk <telesyk89@gmail.com>
2022-10-20 20:10:55 +0000

    Refactor JS: make clear way with trigger actions

--- js/index.js
+++ js/index.js
@@ -4,25 +4,47 @@ const getDataTarget = (target) => document.getElementById(target);
 
 /* Constants */
 const cssMenuVar = '--main-menu-position-x';
+const rootElement = document.documentElement;
+const mainMenuElement = document.getElementById('mainMenu');
+
+const menuOpen = (menu) => {
+    rootElement.style.setProperty(cssMenuVar, '0');
+    menu.dataset.isVisible = true;
+}
+
+const menuClose = (menu) => {
+    rootElement.style.setProperty(cssMenuVar, '-100vh');
+    menu.dataset.isVisible = false;
+}
 
 /* Menu Handler */
 function handleMenu() {
     const menuElement = getDataTarget(this.dataset.target);
     const isVisible = menuElement.dataset.isVisible.trim() || false;
-    const rootElement = document.documentElement;
+    
+    isVisible === 'false' ? menuOpen(menuElement) : menuClose(menuElement);
+}
+
+/* Modal Handler */
+function handleModal() {
+    const modalElement = getDataTarget(this.dataset.target);
+    const isVisible = modalElement.dataset.isVisible.trim() || false;
     
     if (isVisible === 'false') {
-        rootElement.style.setProperty(cssMenuVar, '0');
-        menuElement.dataset.isVisible = true;
+        modalElement.classList.add('is-visible');
+        modalElement.dataset.isVisible = true;
+        menuClose(mainMenuElement); /* In case modal called from menu we need to close it */
     } else {
-        rootElement.style.setProperty(cssMenuVar, '-100vh');
-        menuElement.dataset.isVisible = false;
+        modalElement.classList.remove('is-visible');
+        modalElement.dataset.isVisible = false;
     }
 }
 
-/* Main Menu trigger */
-const menuOpenTrigger = document.getElementById('mainMenuOpen');
-const menuCloseTrigger = document.getElementById('mainMenuClose');
+/* Main Menu Triggers */
+const menuTriggers = document.querySelectorAll('[data-trigger="menu"]');
+menuTriggers.forEach(trigger => trigger.addEventListener('click', handleMenu, false));
 
-menuOpenTrigger.addEventListener('click', handleMenu, false);
-menuCloseTrigger.addEventListener('click', handleMenu, false);
+/* Modal Trigger */
+const modalTriggers = document.querySelectorAll('[data-trigger="modal"]');
+modalTriggers.forEach(trigger => trigger.addEventListener('click', handleModal, false));
+
