f353bdff3612278e6baedb1c2d636ac0b8808e02
Oles Tkachuk <telesyk89@gmail.com>
2022-10-20 18:28:53 +0000

    Finish Header: nav and menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-console.log("Hello Elena's Store");
+/* Main JS */
+/* Helper getDataTarget */
+const getDataTarget = (target) => document.getElementById(target);
+
+/* Constants */
+const cssMenuVar = '--main-menu-position-x';
+
+/* Menu Handler */
+function handleMenu() {
+    const menuElement = getDataTarget(this.dataset.target);
+    const isVisible = menuElement.dataset.isVisible.trim() || false;
+    const rootElement = document.documentElement;
+    
+    if (isVisible === 'false') {
+        rootElement.style.setProperty(cssMenuVar, '0');
+        menuElement.dataset.isVisible = true;
+    } else {
+        rootElement.style.setProperty(cssMenuVar, '-100vh');
+        menuElement.dataset.isVisible = false;
+    }
+}
+
+/* Main Menu trigger */
+const menuOpenTrigger = document.getElementById('mainMenuOpen');
+const menuCloseTrigger = document.getElementById('mainMenuClose');
+
+menuOpenTrigger.addEventListener('click', handleMenu, false);
+menuCloseTrigger.addEventListener('click', handleMenu, false);
